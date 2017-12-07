b2c988d88e81943ad10494412f31cc71a48c5d71
Raphael Luba <raphael@leanbyte.com>
2017-12-07 12:05:36 +0100

    Fix: Don’t flag "joi.only"

--- rules/leftover-only.js
+++ rules/leftover-only.js
@@ -1,11 +1,17 @@
 'use strict';
 
+const disallowedNames = [
+	'describe',
+	'it',
+];
+
 module.exports = function(context) {
     return {
         'CallExpression': function(node) {
             var callee = node.callee;
 
-            if (callee.type === 'MemberExpression' && callee.property.name === 'only') {
+            if (callee.type === 'MemberExpression' && callee.property.name === 'only' &&
+            		disallowedNames.includes(callee.object.name)) {
                 context.report(node, 'Leftover .only() detected');
             }
         }
